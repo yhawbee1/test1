86ecc0c8f4e812acba1c21adbfef3cc4402e9d69
yhawbee1 <antwibrian82@gmail.com>
2023-09-21 18:29:47 +0000

    Update

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const About = () => {
   return (
     <div id='about' className='h-[70vh] w-full bg-violet-400 text-4xl flex justify-center items-center'>
-        About
+        About Brian
     </div>
   )
 }
